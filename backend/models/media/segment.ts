f2127676c6c288edeac24ca77b18d79558e01b34
Jonathan <Jonathan.197ariza@gmail.com>
2023-06-18 23:50:47 -0500

    UUIDv3 added for search sentence function

--- backend/models/media/segment.ts
+++ backend/models/media/segment.ts
@@ -6,8 +6,12 @@ import {
   ForeignKey,
   BelongsTo,
   HasMany,
+  BeforeCreate,
 } from "sequelize-typescript";
+import { Season } from "./season";
 import { Episode } from "./episode";
+import { v3 as uuidv3 } from "uuid";
+import { Media } from "./media";
 
 @Table({
   timestamps: false,
@@ -21,6 +25,12 @@ export class Segment extends Model {
     autoIncrement: true,
   })
   id!: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  uuid!: string;
 
   @Column({
     type: DataType.INTEGER,
@@ -79,6 +89,36 @@ export class Segment extends Model {
 
   @BelongsTo(() => Episode)
   episode!: Episode;
-  
-  
+
+  @BeforeCreate
+  static async generateUUID(instance: Segment) {
+    const media = await Media.findOne({
+      include: [
+        {
+          model: Season,
+          include: [
+            {
+              model: Episode,
+              where : {
+                id: instance.episodeId 
+              },
+              include: [
+                {
+                  model: Segment,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    if (media) {
+      // Generate UUIDv3 based on the english_name, season, episode and the position of segment
+      const uuidNamespace:string | undefined = process.env.UUID_NAMESPACE?.toString();
+      const unique_base_id = `${media?.english_name}-${media?.season[0].number}-${media?.season[0].episode[0].number}-${instance.position}`
+      instance.uuid = uuidv3(unique_base_id,
+        uuidNamespace!
+      );
+    }
+  }
 }
