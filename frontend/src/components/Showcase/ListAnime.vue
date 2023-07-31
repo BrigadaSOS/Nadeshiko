6a4f10867e62fa6c2a817193d45c1af15252fe85
Jonathan <Jonathan.197ariza@gmail.com>
2023-07-30 21:57:49 -0500

    Update ListAnime.vue

--- frontend/src/components/Showcase/ListAnime.vue
+++ frontend/src/components/Showcase/ListAnime.vue
@@ -18,7 +18,7 @@ const getLatestAnime = async () => {
       }
     })
     response = await response.json()
-    latest_anime_list.value = response.results.slice(0,10)
+    latest_anime_list.value = response.results.slice(0, 10)
   } catch (error) {
     console.log(error)
     return
@@ -31,7 +31,7 @@ onMounted(() => {
 </script>
 <template>
   <BatchSearchModal />
-  <div class="max-w-[185rem] px-4 sm:px-6 lg:px-0 lg:py-5 mx-auto ">
+  <div class="max-w-[185rem] px-4 sm:px-6 lg:px-0 lg:py-5 mx-auto">
     <!-- Grid -->
     <div class="grid md:grid-cols-7 gap-10 mb-5 lg:mb-0">
       <div class="md:col-span-3 rounded-lg">
@@ -161,7 +161,6 @@ onMounted(() => {
           <BaseIcon :path="mdiStarShootingOutline" w="w-5 md:w-5" h="h-5 md:h-5" size="20" class="mr-3" />
           <div class="mr-2">Soporte con Anki (Próximamente)</div>
         </button>
-        
       </div>
 
       <!-- End Col -->
@@ -173,28 +172,28 @@ onMounted(() => {
             <div class="inline-flex justify-between items-center w-full mb-4">
               <h1 class="text-2xl font-bold md:text-3xl md:leading-tight dark:text-white">Recién añadido</h1>
               <router-link to="/anime/all">
-              <button
-                type="button"
-                class="border-transparent dark:bg-sgray dark:hover:bg-sgrayhover hs-dropdown-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-300 dark:hover:text-white dark:focus:ring-offset-gray-800"
-              >
-                Ver todos
-                <svg
-                  class="w-2.5 h-auto"
-                  width="17"
-                  height="16"
-                  viewBox="0 0 17 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                <button
+                  type="button"
+                  class="border-transparent dark:bg-sgray dark:hover:bg-sgrayhover hs-dropdown-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-300 dark:hover:text-white dark:focus:ring-offset-gray-800"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M1 7C0.447715 7 -3.73832e-07 7.44771 -3.49691e-07 8C-3.2555e-07 8.55228 0.447715 9 1 9L13.0858 9L7.79289 14.2929C7.40237 14.6834 7.40237 15.3166 7.79289 15.7071C8.18342 16.0976 8.81658 16.0976 9.20711 15.7071L16.0303 8.88388C16.5185 8.39573 16.5185 7.60427 16.0303 7.11612L9.20711 0.292893C8.81658 -0.0976318 8.18342 -0.0976318 7.79289 0.292893C7.40237 0.683417 7.40237 1.31658 7.79289 1.70711L13.0858 7L1 7Z"
-                    fill="currentColor"
-                  />
-                </svg>
-              </button>
-            </router-link>
+                  Ver todos
+                  <svg
+                    class="w-2.5 h-auto"
+                    width="17"
+                    height="16"
+                    viewBox="0 0 17 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M1 7C0.447715 7 -3.73832e-07 7.44771 -3.49691e-07 8C-3.2555e-07 8.55228 0.447715 9 1 9L13.0858 9L7.79289 14.2929C7.40237 14.6834 7.40237 15.3166 7.79289 15.7071C8.18342 16.0976 8.81658 16.0976 9.20711 15.7071L16.0303 8.88388C16.5185 8.39573 16.5185 7.60427 16.0303 7.11612L9.20711 0.292893C8.81658 -0.0976318 8.18342 -0.0976318 7.79289 0.292893C7.40237 0.683417 7.40237 1.31658 7.79289 1.70711L13.0858 7L1 7Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </button>
+              </router-link>
             </div>
 
             <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-6 gap-y-3">
@@ -204,8 +203,13 @@ onMounted(() => {
                 class="w-full relative"
               >
                 <Popper class="w-full" zIndex="50" arrow v-bind="$attrs" hover openDelay="0" closeDelay="0">
-                  <div class="border-none pb-[145%] rounded-md overflow-hidden relative bg-[rgba(255,255,255,0.1)] block">
-                    <img class="w-full h-full object-cover absolute top-0 left-0" :src="item.media_info.cover+'?width=230&height=326'" />
+                  <div
+                    class="border-none pb-[145%] rounded-md overflow-hidden relative bg-[rgba(255,255,255,0.1)] block"
+                  >
+                    <img
+                      class="w-full h-full object-cover absolute top-0 left-0"
+                      :src="item.media_info.cover + '?width=230&height=326'"
+                    />
                   </div>
 
                   <template #content>
@@ -229,11 +233,11 @@ onMounted(() => {
                           </p>
                           <p>
                             <span class="font-bold pt-3 first:pt-0 dark:text-white">Temporadas: </span>
-                            {{item.media_info.num_seasons}}
+                            {{ item.media_info.num_seasons }}
                           </p>
                           <p>
                             <span class="font-bold pt-3 first:pt-0 dark:text-white">Episodios: </span>
-                            {{item.media_info.num_episodes}}
+                            {{ item.media_info.num_episodes }}
                           </p>
                           <p>
                             <span class="font-bold pt-3 first:pt-0 dark:text-white break-words">Generos: </span>
@@ -263,7 +267,6 @@ onMounted(() => {
                 </div>
                 <div class="text-center mt-1 justify-center flex flex-col items-center">
                   <div class="h-2.5 bg-gray-200 rounded-full dark:bg-sgray w-28 mb-4"></div>
-
                 </div>
               </div>
             </div>
@@ -276,31 +279,27 @@ onMounted(() => {
     <!-- End Grid -->
     <div class="my-10 border-b border-white/20" />
 
-<section class="text-white body-font mt-10 ">
-  <h2 class="text-2xl m-4 -mt-5 font-bold md:text-xl md:leading-tight dark:text-white">Estádisticas</h2>
 
-    <div class="mx-auto -mb-20">
-
-      <div class="flex flex-wrap -m-4 text-center">
-        
-        <div class="p-4 md:w-2/4 sm:w-1/2 w-full">
-          <div class=" bg-white/5 border-none px-4 py-6 rounded-lg">
-            <h2 class="title-font font-medium text-3xl text-white">+100K</h2>
-            <p class="leading-relaxed">Oraciones</p>
+  </div>
+  <section class="text-white body-font">
+      <h2 class="text-2xl m-4 -mt-8 font-bold md:text-xl md:leading-tight dark:text-white">Estádisticas generales</h2>
+      <div class="mx-auto -mb-10">
+        <div class="flex flex-wrap -m-4 text-center">
+          <div class="p-4 md:w-2/4 sm:w-1/2 w-full">
+            <div class="bg-white/5 border-none px-4 py-6 rounded-lg">
+              <h2 class="title-font font-medium text-3xl text-white">+100K</h2>
+              <p class="leading-relaxed">Oraciones</p>
+            </div>
           </div>
-        </div>
-        <div class="p-4 md:w-2/4 sm:w-1/2 w-full">
-          <div class=" bg-white/5 border-none px-4 py-6 rounded-lg">
-            <h2 class="title-font font-medium text-3xl text-white">+20</h2>
-            <p class="leading-relaxed">Animes, películas y dramas</p>
+          <div class="p-4 md:w-2/4 sm:w-1/2 w-full">
+            <div class="bg-white/5 border-none px-4 py-6 rounded-lg">
+              <h2 class="title-font font-medium text-3xl text-white">+20</h2>
+              <p class="leading-relaxed">Animes, películas y dramas</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-</section>
-  </div>
-
-
+    </section>
 </template>
 <style>
 :root {
@@ -318,6 +317,10 @@ onMounted(() => {
   margin: 0 !important;
 }
 
-.b{fill:none;stroke:#ffffff;stroke-linecap:round;stroke-linejoin:round;}
+.b {
+  fill: none;
+  stroke: #ffffff;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
 </style>
-
