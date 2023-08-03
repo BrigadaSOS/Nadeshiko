52236676039fde2ffa489dae48dd3c96d91552db
David Afonso <davafons@gmail.com>
2023-08-04 01:21:51 +0900

    Use absolute paths on pgsync scripts

--- backend/config/pgsync/runserver.sh
+++ backend/config/pgsync/runserver.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-./pgsync/wait-for-it.sh $PG_HOST:$PG_PORT -t 60
+/code/pgsync/wait-for-it.sh $PG_HOST:$PG_PORT -t 60
 
-./pgsync/wait-for-it.sh $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT -t 60
+/code/pgsync/wait-for-it.sh $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT -t 60
 
-./pgsync/wait-for-it.sh $REDIS_HOST:$REDIS_PORT -t 60
+/code/pgsync/wait-for-it.sh $REDIS_HOST:$REDIS_PORT -t 60
 
-bootstrap --config ./pgsync/schema.json
+bootstrap --config /code/pgsync/schema.json
 
-pgsync --config ./pgsync/schema.json --daemon
+pgsync --config /code/pgsync/schema.json --daemon
