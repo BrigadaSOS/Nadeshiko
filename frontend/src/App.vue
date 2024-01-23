6acda2762b291135b6afaa8d39a893e26b9ebe2d
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-23 15:00:51 -0500

    Default SEO meta added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <script>
 import { RouterView } from 'vue-router'
 import { initFlowbite } from 'flowbite'
-
+import { useHead } from '@vueuse/head'
+import { computed, reactive } from 'vue'
 export default {
   created() {
     const savedLanguage = localStorage.getItem('language')
@@ -9,6 +10,21 @@ export default {
       this.$i18n.locale = savedLanguage
     }
     initFlowbite()
+  },
+  setup() {
+    const siteDefault = reactive({
+      title: 'NadeDB',
+      description: 'A powerful tool for searching japanese sentences or words from multiple contexts.'
+    })
+    useHead({
+      title: computed(() => siteDefault.title),
+      meta: [
+        {
+          name: 'description',
+          content: computed(() => siteDefault.description)
+        }
+      ]
+    })
   }
 }
 </script>
