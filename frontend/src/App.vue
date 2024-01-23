8c4aff278b09b2a71e79dfa92df598fe992c7e3e
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-23 16:35:33 -0500

    Fix meta tags

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,7 @@
 <script>
 import { RouterView } from 'vue-router'
 import { initFlowbite } from 'flowbite'
-import { useHead } from '@vueuse/head'
-import { computed, reactive } from 'vue'
+
 export default {
   created() {
     const savedLanguage = localStorage.getItem('language')
@@ -10,21 +9,6 @@ export default {
       this.$i18n.locale = savedLanguage
     }
     initFlowbite()
-  },
-  setup() {
-    const siteDefault = reactive({
-      title: 'NadeDB',
-      description: 'A powerful tool for searching japanese sentences or words from multiple contexts.'
-    })
-    useHead({
-      title: computed(() => siteDefault.title),
-      meta: [
-        {
-          name: 'description',
-          content: computed(() => siteDefault.description)
-        }
-      ]
-    })
   }
 }
 </script>
