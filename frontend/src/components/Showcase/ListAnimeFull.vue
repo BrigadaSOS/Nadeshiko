dd07e86eecdabb2807184ecc8a29edd2a6b70373
Jonathan <Jonathan.197ariza@gmail.com>
2024-02-12 02:12:54 -0500

    PREVIEW - Content list in row layout

--- frontend/src/components/Showcase/ListAnimeFull.vue
+++ frontend/src/components/Showcase/ListAnimeFull.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { mdiStarShootingOutline, mdiTextSearch } from '@mdi/js'
+import { mdiArrowRight, mdiTextSearch } from '@mdi/js'
 import BaseIcon from '../minimal/BaseIcon.vue'
 import BatchSearchModal from '../BatchSearchModal.vue'
 import Popper from 'vue3-popper'
@@ -12,15 +12,20 @@ const base_hover = ref(null)
 
 const getLatestAnime = async () => {
   try {
-    let response = await fetch(import.meta.env.VITE_APP_BASE_URL_BACKEND + 'api/search/anime/info?' + new URLSearchParams({
-        sorted: true
-      }), {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
+    let response = await fetch(
+      import.meta.env.VITE_APP_BASE_URL_BACKEND +
+        'api/search/anime/info?' +
+        new URLSearchParams({
+          sorted: true
+        }),
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        }
       }
-    })
+    )
     response = await response.json()
     latest_anime_list.value = response.results
     console.log(base_hover)
@@ -37,68 +42,94 @@ onMounted(() => {
 <template>
   <section class="w-full p-1 rounded-lg">
     <div class="tab-content md:mx-16 mt-6">
-      <div class="inline-flex justify-between items-center w-full mb-4">
-        <h1 class="text-2xl font-bold md:text-3xl md:leading-tight dark:text-white">{{t("animeList.fullListTitle")}}</h1>
+      <div class="inline-flex justify-between items-center w-full">
+        <h1 class="text-2xl font-bold md:text-3xl md:leading-tight dark:text-white">
+          {{ t('animeList.fullListTitle') }}
+        </h1>
       </div>
 
-      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-6 gap-y-3 mt-2 mb-8">
-        <div v-if="latest_anime_list.length > 0" v-for="(media_info, index) in latest_anime_list" class="w-full relative">
-          <Popper class="w-full" zIndex="50" arrow v-bind="$attrs" hover openDelay="0" closeDelay="0">
-            <div class="border-none pb-[145%] rounded-md overflow-hidden relative bg-[rgba(255,255,255,0.1)] block">
-              <img
-                class="w-full h-full object-cover absolute top-0 left-0"
-                :src="media_info.cover"
-              />
+      <div v-if="latest_anime_list.length > 0" v-for="(media_info, index) in latest_anime_list" class="w-full relative">
+        <div
+          class="flex bg-white rounded-lg dark:bg-sgray2/100 hover:dark:bg-sgray2/90 transition-all dark:border-white/10 border my-6"
+        >
+          <img :src="media_info.banner" class="absolute inset-0 object-cover w-full h-full -z-10 rounded-lg" />
+
+          <div class="relative flex-none w-56 h-80">
+            <img :src="media_info.cover" class="absolute inset-0 object-cover w-full h-full rounded-lg" />
+          </div>
+          <div class="flex-auto p-6">
+            <div class="flex flex-wrap">
+              <h1 class="flex-auto text-xl font-semibold dark:text-gray-50">
+                {{ media_info.english_name }}
+              </h1>
+              <div class="text-lg font-semibold bg-gray-500 px-2 rounded-xl text-white">Anime</div>
+              <div class="flex-none w-full mt-2 text-sm font-medium text-gray-500 dark:text-gray-300">
+                {{ media_info.japanese_name }} - {{ media_info.romaji_name }}
+              </div>
             </div>
 
-            <template #content>
-              <div class="w-full backdrop-blur-sm bg-sgray2/90 flex flex-col max-w-[400px]">
-                <span
-                  class="mx-auto object-center mt-2 text-center px-2 text-lg font-bold text-gray-800 dark:text-white"
-                  >{{ media_info.english_name }}</span
-                >
-                <div class="py-3 px-4 text-sm text-gray-600 dark:text-gray-400 min-w-[400px]">
-                  <div class="pb-[20%] overflow-hidden relative bg-[rgba(255,255,255,0.1)] block">
-                    <img class="object-cover absolute top-0 left-0" :src="media_info.banner" />
+            <div
+              class="mt-2 py-2 flex items-center text-sm text-gray-800 gap-x-1.5 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-0 dark:text-white dark:after:border-white/20"
+            ></div>
+
+            <div class="grid grid-cols-2 mt-2">
+              <p class="text-sm my-1 shadow font-semibold text-gray-500 dark:text-gray-300">
+                Cantidad de Oraciones: {{ media_info.num_segments }}
+              </p>
+
+              <p class="text-sm my-1 shadow font-semibold text-gray-500 dark:text-gray-300">Cantidad de Palabras:</p>
+              <p class="text-sm my-1 shadow font-semibold text-gray-500 dark:text-gray-300">Palabras Únicas:</p>
+              <p class="text-sm my-1 shadow font-semibold text-gray-500 dark:text-gray-300">Kanjis Únicos:</p>
+              <p class="text-sm my-1 shadow font-semibold text-gray-500 dark:text-gray-300">
+                Palabras Únicas (usadas una vez):
+              </p>
+              <p class="text-sm my-1 shadow font-semibold text-gray-500 dark:text-gray-300">
+                Índice de Diversidad Léxica:
+              </p>
+            </div>
+
+            <div class="flex absolute bottom-8">
+              <div class="flex">
+                <div class="flex flex-row bg-white border shadow-sm rounded-xl dark:bg-graypalid dark:border-gray-800">
+                  <div class="px-2 py-2 text-center">
+                    <div class="flex items-center gap-x-2">
+                      <p class="text-xs uppercase tracking-wide text-white">Temporadas:</p>
+                      <p class="text-xs font-medium text-gray-800 dark:text-gray-200">{{ media_info.num_seasons }}</p>
+                    </div>
                   </div>
-                  <div class="mt-3 break-words">
-                    <p>
-                      <span class="font-bold pt-3 first:pt-0 dark:text-white">{{t("animeList.romajiName")}}: </span>
-                      {{ media_info.romaji_name }}
-                    </p>
-                    <p>
-                      <span class="font-bold pt-3 first:pt-0 dark:text-white">{{t("animeList.japaneseName")}}: </span>
-                      {{ media_info.japanese_name }}
-                    </p>
-                    <p>
-                      <span class="font-bold pt-3 first:pt-0 dark:text-white">{{t("animeList.seasons")}}: </span>
-                      {{media_info.num_seasons}}
-                    </p>
-                    <p>
-                      <span class="font-bold pt-3 first:pt-0 dark:text-white">{{t("animeList.episodes")}}: </span>
-                      {{media_info.num_episodes}}
-                    </p>
-                    <p>
-                      <span class="font-bold pt-3 first:pt-0 dark:text-white break-words">{{t("animeList.genres")}}: </span>
-                      {{ media_info.genres.toString() }}
-                    </p>
+                </div>
+
+                <div class="flex flex-row bg-white border shadow-sm rounded-xl dark:bg-graypalid dark:border-gray-800">
+                  <div class="px-2 py-2 text-center">
+                    <div class="flex items-center gap-x-2">
+                      <p class="text-xs uppercase tracking-wide text-white">Episodios:</p>
+                      <p class="text-xs font-medium text-gray-800 dark:text-gray-200">{{ media_info.num_episodes }}</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </template>
-          </Popper>
+            </div>
 
-          <div class="mt-2 text-center justify-center flex flex-col items-center">
-            <h3 class="text-sm text-center font-semibold line-clamp-2">{{ media_info.english_name }}</h3>
+            <div
+              class="mt-2 py-2 flex items-center text-sm text-gray-800 gap-x-1.5 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-0 dark:text-white dark:after:border-white/20"
+            ></div>
+
+            <div class="flex absolute bottom-0 right-4">
+              <button
+                type="button"
+                data-hs-overlay="#hs-vertically-centered-scrollable-batch1"
+                class="py-3.5 ml-auto duration-300 px-4 mb-4 inline-flex justify-center items-center gap-2 border font-medium shadow-sm align-middle  transition-all text-sm text-gray-900 rounded-lg focus:border-red-500 dark:border-blue-400 dark:placeholder-gray-400 dark:text-blue-400"
+              >
+                <div class="">Detalles</div>
+                <BaseIcon :path="mdiArrowRight" w="w-5 md:w-5" h="h-5 md:h-5" size="20" class="" />
+              </button>
+            </div>
           </div>
-          <div class="text-center mt-1 justify-center flex flex-col items-center">
-            <h3 class="text-sm text-center font-medium line-clamp-2">{{ media_info.num_segments }} {{t("animeList.sentenceCount")}}</h3>
-          </div>
-        </div>
-        <div v-else role="status" v-for="i in 10" class="animate-pulse relative">
-          <div class="w-full pb-[145%] items-center overflow-hidden relative bg-[rgba(255,255,255,0.1)] block"></div>
         </div>
       </div>
     </div>
   </section>
 </template>
+<style>
+
+</style>
