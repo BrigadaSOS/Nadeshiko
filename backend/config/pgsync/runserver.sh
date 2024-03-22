95e1cb2fd78038cde8d9f45e94cd070fc75e3e2f
Jonathan <Jonathan.197ariza@gmail.com>
2024-03-22 10:47:14 -0500

    Remove port s

--- backend/config/pgsync/runserver.sh
+++ backend/config/pgsync/runserver.sh
@@ -2,7 +2,7 @@
 
 /code/pgsync/wait-for-it.sh $PG_HOST:$PG_PORT -t 60
 
-/code/pgsync/wait-for-it.sh $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT -t 60
+/code/pgsync/wait-for-it.sh $ELASTICSEARCH_HOST -t 60
 
 /code/pgsync/wait-for-it.sh $REDIS_HOST:$REDIS_PORT -t 60
 
