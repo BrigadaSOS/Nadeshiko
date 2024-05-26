569a346087f0dc79a31718f719a358dfe9ddf622
Jonathan <Jonathan.197ariza@gmail.com>
2024-05-26 17:02:57 -0500

    Category filter working

--- frontend_v2/components/search/SearchContainer.vue
+++ frontend_v2/components/search/SearchContainer.vue
@@ -1,74 +1,47 @@
 <script setup>
-import { useI18n } from '#imports'
 const { t } = useI18n()
-
 const apiSearch = useApiSearch();
 const route = useRoute();
 
+// Main variables
 let searchData = ref(null);
 let isLoading = ref(false);
 
 // Available params for search
 let query = ref('');
-let category = ref(0)
+let category = ref(0);
 let cursor = ref(null);
 
-// Customization section 
+// Category mapping
 const categoryMapping = {
     'all': 0,
     'anime': 1,
     'liveaction': 3
-}
-const reverseCategoryMapping = Object.fromEntries(Object.entries(categoryMapping).map(([key, value]) => [value, key]));
-
-onMounted(async () => {
-    // 1. Get all params for make first search
-    query.value = route.query.query;
-    category.value = categoryMapping[route.query.category] ?? null
-
-    // 2. Get all sentences
-    await fetchSentences();
-
-    // Logic for infinite scroll
-    window.addEventListener('scroll', handleScroll);
-    window.HSStaticMethods.autoInit();
-});
-
-onBeforeRouteUpdate(async (to, from) => {
-    query.value = to.query.query;
-
-    // We must clear results from before
-    cursor.value = null
-
-    await fetchSentences();
-
-})
-
-onBeforeUnmount(() => {
-    window.removeEventListener('scroll', handleScroll);
-});
+};
 
 // Fetch sentences with an infinite scroll
 const fetchSentences = async () => {
     try {
         isLoading.value = true;
 
-        // Initial query
+        // Build request body
         let body = {
             query: query.value,
             limit: 10
-        }
+        };
 
-        // Optional params
-        if (category.value !== null) {
-            body.category = [category.value]
+        // Add optional parameters, omit category if it is 'all'
+        if (category.value !== 0) {
+            body.category = [category.value];
         }
         if (cursor.value) {
-            body.cursor = cursor.value
+            body.cursor = cursor.value;
         }
 
+        // Fetch data from API
         const response = await apiSearch.getSentenceV1(body);
 
+        // Update search data
         if (cursor.value === null) {
             searchData.value = response;
         } else {
@@ -76,7 +49,6 @@ const fetchSentences = async () => {
         }
 
         cursor.value = response.cursor;
-
     } catch (error) {
         console.error('Error fetching sentences:', error);
     } finally {
@@ -84,28 +56,63 @@ const fetchSentences = async () => {
     }
 };
 
-
-// Logic for infinite scroll
+// Handle scroll event for infinite scroll
 const handleScroll = () => {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !isLoading.value) {
         fetchSentences();
     }
 };
 
+// Get count of sentences for a specific category
 const getCategoryCount = (category) => {
-    if (category == 0) {
+    if (category === 0) {
         return searchData.value.categoryStatistics.reduce((total, item) => total + item.count, 0);
     }
     const item = searchData.value.categoryStatistics.find(item => item.category === category);
     return item ? item.count : 0;
 };
 
+// Filter sentences by category
 const categoryFilter = async (filter) => {
-    category.value = filter
-    window.scrollTo(0, 0)
-}
+    category.value = filter;
+    cursor.value = null;
+    await fetchSentences();
+    window.scrollTo(0, 0);
+};
+
+// Lifecycle hooks
+onMounted(async () => {
+    query.value = route.query.query;
+    category.value = categoryMapping[route.query.category] ?? 0;
+
+    if (category.value === undefined) {
+        category.value = 0;
+    }
+
+    await fetchSentences();
+
+    window.addEventListener('scroll', handleScroll);
+    window.HSStaticMethods.autoInit();
+});
+
+onBeforeRouteUpdate(async (to, from) => {
+    query.value = to.query.query;
+    category.value = categoryMapping[to.query.category] ?? 0;
+
+    if (category.value === undefined) {
+        category.value = 0;
+    }
+
+    cursor.value = null;
+    await fetchSentences();
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
 
 </script>
+
 <template>
     <div class="flex-1 mx-auto">
         <!-- Tabs -->
@@ -113,7 +120,7 @@ const categoryFilter = async (filter) => {
             <GeneralTabsContainer>
                 <GeneralTabsHeader>
                     <GeneralTabsItem category="0" categoryName="Todo" :count="getCategoryCount(0)"
-                        :isActive="category === 0 || category == null " @click="categoryFilter(0)" />
+                        :isActive="category === 0" @click="categoryFilter(0)" />
                     <GeneralTabsItem category="1" categoryName="Anime" :count="getCategoryCount(1)"
                         :isActive="category === 1" @click="categoryFilter(1)" />
                     <GeneralTabsItem category="3" categoryName="Liveaction" :count="getCategoryCount(3)"
@@ -134,7 +141,6 @@ const categoryFilter = async (filter) => {
             <div class="pl-4 mx-auto hidden 2xl:block">
                 <SearchSegmentFilterContent :searchData="searchData" :categorySelected="category" />
             </div>
-
         </div>
     </div>
 </template>
