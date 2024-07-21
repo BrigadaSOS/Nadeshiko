fe8643806f561c773c8da82ac77b6a156d40cea6
Jonathan <Jonathan.197ariza@gmail.com>
2024-07-20 21:22:13 -0500

    dropdown overlapping fixed

--- frontend_v2/components/search/dropdown/DropdownContainer.vue
+++ frontend_v2/components/search/dropdown/DropdownContainer.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="hs-dropdown relative z-50 inline-flex">
+    <div class="hs-dropdown relative inline-flex">
         <slot></slot>
         <div
-            class="hs-dropdown-menu items-center text-center align-middle transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md  p-2 mt-2 dark:bg-neutral-800 border-none rounded-lg"
+            class="hs-dropdown-menu absolute z-50 items-center text-center align-middle transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md p-2 mt-2 dark:bg-neutral-800 border-none rounded-lg"
             :aria-labelledby="dropdownId"
         >
             <slot name="content"></slot>
