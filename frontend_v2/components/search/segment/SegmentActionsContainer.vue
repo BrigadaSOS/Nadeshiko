531d4ac19772640bd281a12a7aea4a6a3004d138
xyaman <dbarriapaiva@gmail.com>
2024-08-01 00:09:01 +0900

    Initial support for adding card by query

--- frontend_v2/components/search/segment/SegmentActionsContainer.vue
+++ frontend_v2/components/search/segment/SegmentActionsContainer.vue
@@ -9,53 +9,15 @@ type Props = {
 
 let props = defineProps<Props>();
 
-const emit = defineEmits(['open-context-modal']);
+const emit = defineEmits(['open-context-modal', 'open-anki-modal']);
 
 const openContextModal = () => {
   emit('open-context-modal', props.content);
 };
 
-// addSentenceToAnki function is used to add a sentence to Anki
-// It sends a message to the background script with the sentence and the id of the note to update.
-// If the id is null, it will use the id of the last note added.
-const addSentenceToAnki = (sentence: Sentence, id: number | null = null) => {
- // const toastOptions = {
- //   timeout: 3000,
- //   position: 'bottom-right'
- // };
-
-  const localSettings = localStorage.getItem('settings');
-  if (!localSettings) {
-    const message = 'No se han encontrado ajustes. Por favor, vaya a la página de ajustes y configure la extensión.'
-    // throw new Error('No se han encontrado ajustes. Por favor, vaya a la página de ajustes y configure la extensión.');
-    alert(message);
-    return;
-  }
-
-  const settings = JSON.parse(localSettings)
-  const config = useRuntimeConfig();
-  const extensionId = config.public.NUXT_APP_EXTENSION_KEY;
-  console.log(extensionId);
-  const request = {
-    action: 'updateAnkiCard',
-    settings: settings,
-    sentence: sentence,
-    id: id,
-  }
-
-  chrome.runtime.sendMessage(extensionId, request, (response) => {
-    console.log(response)
-    if (response.error) {
-      const message = 'No se ha podido añadir la tarjeta en Anki. Error: ' + response.error
-      throw new Error(message)
-    } else {
-      const message = 'La tarjeta ha sido añadida en Anki'
-      //toast.success(message, options)
-      alert(message);
-    }
-  })
+const openAnkiModal = () => {
+  emit('open-anki-modal');
 };
-
 
 </script>
 <template>
@@ -77,8 +39,9 @@ const addSentenceToAnki = (sentence: Sentence, id: number | null = null) => {
 
         <!-- Anki by ID -->
         <SearchDropdownItem 
-          text="Añadir a Anki (Por note ID)"
+          text="Añadir a Anki (Busca en tu collección)"
           :iconPath="mdiStarShootingOutline" 
+          @click="openAnkiModal"
         />
       </SearchDropdownContent>
     </template>
