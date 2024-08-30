40c1ce91977fc4127698aac9cc72841f3cc629c7
Jonathan <Jonathan.197ariza@gmail.com>
2024-08-30 18:50:16 -0500

    Old frontend deleted

--- frontend/src/components/ErrorConnection.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { useI18n } from 'vue-i18n'
-const { t } = useI18n()
-</script>
-
-<template>
-  <p class="text-white text-2xl text-center m-10">{{t("searchpage.main.labels.errorMessage1")}}<br/>{{t("searchpage.main.labels.errorMessage2")}}</p>
-</template>
--- frontend/src/utils/misc.js
+++
@@ -1,9 +0,0 @@
-export function normalizeSentence(string) {
-  if(!string) return "";
-
-  string = string.trim()
-  if (string.endsWith(',')) {
-    string = string.slice(0, -1)
-  }
-  return string.charAt(0).toUpperCase() + string.slice(1)
-}
