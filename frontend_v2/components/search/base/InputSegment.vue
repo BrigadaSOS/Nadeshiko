7ce6f17ff1c49e3f15d0d4ce86cb354a5466ff7a
xyaman <dbarriapaiva@gmail.com>
2024-10-29 11:50:24 +0900

    Fixed: Shift + s keybind

--- frontend_v2/components/search/base/InputSegment.vue
+++ frontend_v2/components/search/base/InputSegment.vue
@@ -13,9 +13,18 @@ const navigateSearchSentence = async () => {
   })
 }
 
-const handleKeyDown = (event) => {
+const handleKeyDown = event => {
   if (event.shiftKey && event.key === 'S') {
-    window.scrollTo({ top: 0, behavior: 'instant' })
+
+    const inputElem = document.getElementById("sentence-search-input");
+    const rect = inputElem.getBoundingClientRect();
+
+    // The element is no longer visible
+    if (rect.bottom <= 0) {
+      event.preventDefault();
+      window.scrollTo({ top: 0, behavior: 'instant' })
+      inputElem.focus();
+    }
   }
 }
 
@@ -37,7 +46,7 @@ onBeforeUnmount(() => {
       <div class="flex-[1_0_0%] ">
         <label for="hs-search-article-1" class="block text-sm text-gray-700 font-medium dark:text-white"><span
             class="sr-only">Search anything!</span></label>
-        <input autofocus v-model="query" @keydown.enter="navigateSearchSentence"
+        <input id="sentence-search-input" autofocus v-model="query" @keydown.enter="navigateSearchSentence"
           class=" dark:focus:ring-gray-500 border py-3 dark:focus:border-gray-500 h-full px-4 block w-full border-transparent rounded-lg  focus:outline-none dark:bg-input-background dark:border-neutral-600 dark:text-white/80 dark:placeholder-neutral-500"
           placeholder="Search anything!" />
         <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none  pe-36">
