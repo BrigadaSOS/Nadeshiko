65f0e61a8fabfdd676a582fecd5162a057236c57
Jonathan <Jonathan.197ariza@gmail.com>
2025-03-01 21:51:52 +0900

    Revert "Patreon removed"
    This reverts commit cb36d48be392809448cb64f329f579c608a19465.

--- frontend_v2/pages/index.vue
+++ frontend_v2/pages/index.vue
@@ -360,12 +360,10 @@ watch(() => window.innerWidth, checkScreenSize);
                                         </a>
                                     </div>
                                     <div class="mt-5 w-auto">
-                                        <!--
                                         <a href="https://patreon.com/BrigadaSOS">
                                             <img class="h-12 object-contain rounded-md" src="/patreon.png"
                                                 alt="Become a Patron" />
                                         </a>
-                                        -->
                                     </div>
                                 </div>
                             </div>
